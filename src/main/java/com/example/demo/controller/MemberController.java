87920c75ebcf8a3fbfc9579ea3faa672c0234b3b
codejcd <codejcd@gmail.com>
2022-09-09 23:29:31 +0900

    test: 도메인 클래스 컨번터
    페이징 리턴 예제 개발

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +25,17 @@ public class MemberController {
 		return member.getUsername();
 	}
 	
+	@GetMapping("/members")
+	public Page<Member> list(Pageable pageable) {
+		return memberRepository.findAll(pageable);
+	}
+	
 	@PostConstruct
 	public void init() {
-		memberRepository.save(new Member("userA"));
+		//memberRepository.save(new Member("userA"));
+		for(int i=0; i<100; i++) {
+			//memberRepository.save(new Member("userA"));
+			memberRepository.save(new Member("user" + i, i));
+		}	
 	}
 }
